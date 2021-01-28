d08b8027116634bb51dc80d9de27a454c7ed2e54
Jennifer Qi <jeqi@ucsd.edu>
2021-01-28 10:57:34 -0800

    did Lab 4

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,33 @@
 exports.view = function(req, res){
   res.render('index');
 };
+
+exports.view = function(req, res){
+	res.render("index", {
+		"name": "Waiting in Line",
+		"image": "lorempixel.people.1.jpeg",
+		"id": "project1"
+	});
+};
+
+exports.view = function(req, res) {
+	res.render("index", {
+		"projects": [
+		{
+			"name": "Waiting in Line",
+			"image": "lorempixel.people.1.jpeg",
+			"id": "project1"
+		},
+		{
+			"name": "Needfinding",
+			"image": "lorempixel.city.1.jpeg",
+			"id": "project2"
+		},
+		{
+			"name": "Prototyping",
+        	"image": "lorempixel.technics.1.jpeg",
+        	"id": "project3"
+		}
+		]
+	});
+};
